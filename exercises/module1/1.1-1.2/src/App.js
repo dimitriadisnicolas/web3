c51d646a3094664c5bf1d6a8543f33543761b259
dimitriadisnicolas <nicolas.dimitriadis@student.vinci.be>
2022-09-19 18:07:18 +0200

    1.1-1.2 : refactor - modules

--- exercises/module1/1.1-1.2/src/App.js
+++ exercises/module1/1.1-1.2/src/App.js
@@ -1,7 +1,34 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-);
+import Header from "./components/Header/Header.jsx";
+import Content from "./components/Content/Content.jsx";
+import Total from "./components/Total/Total.jsx";
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <p>
+        <Content
+          part1={part1}
+          exercises1={exercises1}
+          part2={part2}
+          exercises2={exercises2}
+          part3={part3}
+          exercises3={exercises3}
+        />
+      </p>
+      <p>
+        <Total total={exercises1 + exercises2 + exercises3} />
+      </p>
+    </div>
+  );
+};
 
 export default App;
