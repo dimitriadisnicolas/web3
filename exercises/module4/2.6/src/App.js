db6bac766f9af0d873d23092aab65e5ae211719e
dimitriadisnicolas <nicolas.dimitriadis@student.vinci.be>
2022-10-14 11:49:07 +0200

    feature : ex 2.9 add numbers to phonebook

--- exercises/module4/2.6/src/App.js
+++ exercises/module4/2.6/src/App.js
@@ -1,13 +1,19 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (e) => {
     e.preventDefault();
 
-    if (newName === "") {
+    if (newName === "" || newNumber === "") {
       return alert("Nope u cant do that");
     }
     const exists = persons.filter((person) => person.name === newName);
@@ -17,13 +23,20 @@ const App = () => {
     }
     const personObject = {
       name: newName,
+      number: newNumber,
+      id: persons.length + 1,
     };
     setPersons(persons.concat(personObject));
     setNewName("");
+    setNewNumber("");
   };
 
   const handleNewName = (e) => {
     setNewName(e.target.value);
+  };
+
+  const handleNewNumber = (e) => {
+    setNewNumber(e.target.value);
   };
 
   return (
@@ -34,13 +47,18 @@ const App = () => {
           name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNewNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, index) => (
-          <li key={index}>{person.name}</li>
+        {persons.map((person) => (
+          <li key={person.id}>
+            {person.name} : {person.number}
+          </li>
         ))}
       </ul>
     </div>
